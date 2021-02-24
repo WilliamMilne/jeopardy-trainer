d2fcd92a997748b2ebf4c0744dde09d808fca0a5
James Milne <jamesmilne925@gmail.com>
2021-02-23 20:59:47 -0500

    getting categories of the first round

--- web-scraper/__main__.py
+++ web-scraper/__main__.py
@@ -1,4 +1,16 @@
-import beautifulsoup
+from bs4 import BeautifulSoup
+from j_scraper import j_scraper
+import urllib.request
+
+j_archive_game_url = "https://www.j-archive.com/showgame.php?game_id="
 
 if __name__ == "__main__":
-    pass
+
+    # Get HTML from j-archive
+    fid=urllib.request.urlopen(j_archive_game_url + '1')
+    webpage=fid.read().decode('utf-8')
+    
+    soup = BeautifulSoup(webpage, 'html.parser')
+    j_scraper = j_scraper()
+    j_scraper.get_categories(soup.find(id="jeopardy_round"))
+    print('done')
---
+++ web-scraper/j_scraper.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+
+class j_scraper():
+    """
+    Parses the given j! Archive page and sends all questions to be stored in the database.
+    """
+
+    def __init__(self):
+        """
+
+        """
+        pass
+        
+    def get_categories(self, j_round):
+        """
+
+        """
+        all_td = j_round.find_all('td')
+        categories_html = []
+        for td in all_td:
+            try:
+                categories_html.append(td.td.text)
+            except AttributeError:
+                pass
+        return categories_html
+
+
+    def parse_round(self):
+        """
+
+        """
+        pass
