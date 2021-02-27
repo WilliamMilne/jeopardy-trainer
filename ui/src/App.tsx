0da12392031277131207c8339781915cb89eebb3
William Milne <williammilne@gmail.com>
2021-02-27 13:25:22 -0500

    react app is running and carbon button exists

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+
+import { Button } from 'carbon-components-react'
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button>Button!</Button>
     </div>
   );
 }
